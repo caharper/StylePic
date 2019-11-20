cef631fa8ef3279dce527f944afb650cabeed66b
Harry Holcomb <holcombharry@gmail.com>
2019-11-19 18:46:18 -0600

    Slide Bar.
    Can only select one style. Style will highlight if selected.

--- gui/src/style-slide-item.js
+++ gui/src/style-slide-item.js
@@ -7,22 +7,16 @@ export default class StyleSlideItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            bgColor: "white",
             index: this.props.index
         }
     }
 
     isSelected() {
-        if(this.props.currSelected == this.state.index/*this.state.bgColor == "white"*/) {
-            alert("Hello")
-            this.setState({
-                bgColor: "lightblue"
-            })
+        if(this.props.currSelected == this.state.index) {
+            return "lightblue";
         }
         else {
-            this.setState({
-                bgColor: "white"
-            })
+            return "white";
         }
     }
 
@@ -37,10 +31,10 @@ export default class StyleSlideItem extends React.Component {
                 borderWidth: 1,
                 borderColor: 'grey',
                 margin: 4,
-                backgroundColor: this.state.bgColor,
+                backgroundColor: this.isSelected(),
                 zIndex: 2
                 }}
-                onPress={() => this.isSelected()}>
+                onPress={this.props.setSelected}>
                 <Image 
                     style={{width: 75, height: 75}}
                     source={{uri: this.props.img}}
@@ -51,17 +45,3 @@ export default class StyleSlideItem extends React.Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 7.5,
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: 90,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: 'grey',
-        margin: 4
-    }
-});
