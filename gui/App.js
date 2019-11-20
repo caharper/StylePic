832005e335d30c8b72b07347dae76d51745521de
Harry Holcomb <holcombharry@gmail.com>
2019-11-19 19:51:20 -0600

    Camera Roll
    Able to browse photos. Not able to select one yet.

--- gui/App.js
+++ gui/App.js
@@ -6,6 +6,7 @@ import StylePage from './src/style.page';
 import StyleSlideBar from './src/style-slide-bar'
 import StyleSlideItem from './src/style-slide-item'
 import SelectionPage from './src/selection.page'
+import CameraRollPage from './src/camera.roll'
 import ColRowInputs from './src/col-row-inputs'
 import { STYLES } from './src/style-list'
 
@@ -19,6 +20,7 @@ export default class App extends React.Component {
                 <Route exact path="/" component={CameraPage} />
                 <Route exact path="/style.page" component={StylePage} />
                 <Route exact path="/selection.page" component={SelectionPage} />
+                <Route exact path="/camera.roll.page" component={CameraRollPage} />
             </NativeRouter>
         );
     };
