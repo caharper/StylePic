98aa2f9d004f7461bed8253bfc7ebdaf0a33d88a
Jake Watkins <jakewatkins@beckgroup.com>
2019-11-21 12:19:02 -0600

    testing array

--- style_transfer/routes.py
+++ style_transfer/routes.py
@@ -170,7 +170,7 @@ def upload_file():
 
         # output_img = get_styled_image(filename + '.jpg', selected_styles, num_rows=2, num_cols=1)
         # output_img = get_styled_image(filename, selected_styles, num_rows=2, num_cols=1)
-        output_img = get_styled_image(filename, selected_styles, num_rows=rows, num_cols=cols)
+        output_img = get_styled_image(filename, selected_styles, num_rows=int(rows), num_cols=int(cols))
         output_img.save('./../returnImage' + image_num + '.jpg')
         counter = counter + 1
 
